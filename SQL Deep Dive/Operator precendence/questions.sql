57302f45a2aa876de8ffa2237dd5a527083f6c0f
JoBelmonte <134909686+JoBelmonte@users.noreply.github.com>
2023-05-31 21:50:52 -0700

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -6,6 +6,9 @@
 * Select people either under 30 or over 50 with an income above 50000
 * Include people that are 50
 * that are from either Japan or Australia
+select firstname, age, country, income from "public"."customers"
+where (age < 30 or age >=50) and income > 50000 and (country = 'Japan' or country = 'Australia' )
+868
 */
 
 /*
@@ -14,3 +17,6 @@
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+select sum(totalamount) from "public"."orders"
+where (orderdate >= '2004-06-01' and orderdate <='2004-06-30') and netamount > 100
+203575
