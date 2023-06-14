199c61b15721a8a7de11cad0a957d0a83f946997
JoBelmonte <134909686+JoBelmonte@users.noreply.github.com>
2023-06-13 20:57:37 -0700

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,17 +3,22 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
-
+select first_name, last_name from public.employees
+order by first_name, last_name desc;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees by age
 */
-
+select first_name,last_name, birth_date from public.employees
+order by birth_date asc;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+select first_name,last_name, birth_date from public.employees
+where first_name ILIKE 'K%'
+order by birth_date;
